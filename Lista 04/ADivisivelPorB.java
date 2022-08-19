ff1bb9091f8b90bb0835c00fa439c89eafe57fd0
EuMotta <Hyperxmotta@hotmail.com>
2022-08-19 09:14:10 -0300

    Lista 04 Incompleta

---
+++ Lista 04/ADivisivelPorB.java
@@ -0,0 +1,19 @@
+//José Antonio Motta
+import java.util.Scanner;
+public class ADivisivelPorB {
+    public static void main(String[] args) {
+		Scanner valorA = new Scanner(System.in);
+		System.out.println("Valor de A");
+		int A = valorA.nextInt();
+		Scanner valorB = new Scanner(System.in);
+		System.out.println("Valor de B");
+		int B = valorB.nextInt();
+		if(A < 1 || A > 1000 || B < 1 || B > 1000){
+			System.out.println("INVALIDO");
+			}else if(A % B == 0){
+				System.out.println("Divisivel");
+			}else{
+				System.out.println("Nao Divisivel");
+			}
+    }
+}

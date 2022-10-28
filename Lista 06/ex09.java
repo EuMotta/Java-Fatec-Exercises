9f5673c75a6805a643fa382ef4ca7b6a441d9339
EuMotta <Hyperxmotta@hotmail.com>
2022-10-28 19:45:16 -0300

    ex09

---
+++ Lista 06/ex09.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ex09 {
+    public static void main(String[] args) {
+        int qtdd = 0, qtdf = 0;
+        Scanner num = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+          System.out.println("Digite um número: ");  
+          int numdigitado = num.nextInt();
+          if (numdigitado >= 10 && numdigitado <=20) {
+            qtdd++;
+          }else{
+            qtdf++;
+          }
+        }
+        System.out.println("entre 10 e 20: " + qtdd);
+        System.out.println("Quatidade fora: " + qtdf);
+        
+    }
+}

568a7a6e4571b637a81fc1f3acd1a9f7665bd6be
EuMotta <hyperxmotta@hotmail.com>
2023-03-24 09:22:31 -0300

    POO ex04

---
+++ POO/Lista01/ex04.java
@@ -0,0 +1,36 @@
+/*
+ * ex04.java
+ * 
+ * 
+ */
+
+import java.util.Scanner;
+
+public class ex04 {
+
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		System.out.print("insira a quantidade de linhas: ");
+		int qtdLinha = num.nextInt();
+		System.out.print("insira a quantidade de colunas: ");
+		int qtdColuna = num.nextInt();
+
+		while (qtdLinha <= 0 || qtdColuna <= 0) {
+			System.out.print("insira a quantidade de linhas: ");
+			qtdLinha = num.nextInt();
+			System.out.print("insira a quantidade de colunas: ");
+			qtdColuna = num.nextInt();
+		}
+		int[][] arr = new int[qtdLinha][qtdColuna];
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				int valor = (int) (Math.random() * 100);
+				arr[i][j] = valor;
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		num.close();
+	}
+}

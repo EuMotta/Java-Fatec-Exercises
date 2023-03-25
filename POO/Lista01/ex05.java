cc7066e8205c4fe0878c9c90d2ad200506c01d8a
EuMotta <hyperxmotta@hotmail.com>
2023-03-25 11:10:26 -0300

    POO ex05

---
+++ POO/Lista01/ex05.java
@@ -0,0 +1,48 @@
+/*
+ * ex05.java
+ * 
+ * 
+ * 
+ */
+
+import java.util.Scanner;
+
+public class ex05 {
+
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		System.out.print("digite uma quantidade de linhas: ");
+		int qtdLinhas = num.nextInt();
+		System.out.print("digite uma quantidade de colunas: ");
+		int qtdColunas = num.nextInt();
+		int[][] arr1 = new int[qtdLinhas][qtdColunas], arr2 = new int[qtdLinhas][qtdColunas],
+				arrTotal = new int[qtdLinhas][qtdColunas];
+
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr1[i].length; j++) {
+				int valor = (int) (Math.random() * 100);
+				arr1[i][j] = valor;
+				System.out.print(arr1[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for (int i = 0; i < arr2.length; i++) {
+			for (int j = 0; j < arr2[i].length; j++) {
+				int valor = (int) (Math.random() * 100);
+				arr2[i][j] = valor;
+				System.out.print(arr2[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr1[i].length; j++) {
+				arrTotal[i][j] = arr1[i][j] + arr2[i][j];
+				System.out.print(arrTotal[i][j] + " ");
+			}
+			System.out.println();
+		}
+		num.close();
+	}
+}

47e09afa0eefcd4e8c0ce5ddd0c1a40411d963e1
EuMotta <hyperxmotta@hotmail.com>
2023-04-11 10:11:17 -0300

    ex01

---
+++ POO/Lista05/ex01/Index.java
@@ -0,0 +1,27 @@
+public class Index {
+  public static void main(String[] args) {
+    Lampada lampada = new Lampada("LG", "LED", "Luminaria");
+
+    if(lampada.isEstado()){
+      System.out.println("Lâmpada acesa");
+    }else{
+      System.out.println("Lâmpada apagada");
+    }
+
+    lampada.acender();
+
+    if(lampada.isEstado()){
+      System.out.println("Lâmpada acesa");
+    }else{
+      System.out.println("Lâmpada apagada");
+    }
+
+    lampada.apagar();
+
+    if(lampada.isEstado()){
+      System.out.println("Lâmpada acesa");
+    }else{
+      System.out.println("Lâmpada apagada");
+    }
+  }
+}

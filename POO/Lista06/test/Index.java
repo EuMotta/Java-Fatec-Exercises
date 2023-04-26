a9ffb7fd47eebc907de9ed089d88cdd3687f6a37
EuMotta <hyperxmotta@hotmail.com>
2023-04-25 22:11:34 -0300

    Product edit transfer to ProductMananger

--- POO/Lista06/test/Index.java
+++ POO/Lista06/test/Index.java
@@ -18,14 +18,14 @@ public class Index {
       switch (opcao) {
         /* Adicionar produtos */
         case 1:
-          index.addProduct();
+          product.addProduct();
           break;
         /* Exibir produtos */
         case 2:
-          index.showProducts();
+          product.showProducts();
           break;
         case 3:
-          index.editProduct();
+          product.editProduct();
           break;
         default:
           break;
@@ -33,86 +33,5 @@ public class Index {
     } while (opcao != 4);
     num.close();
   }
-
-  public void showProducts() {
-    System.out.println("Produtos no carrinho:");
-    for (Product produto : listaDeProdutos) {
-      System.out.println("Nome: " + produto.getName());
-      System.out.println("Descrição: " + produto.getDescription());
-      System.out.println("ID: " + produto.getId());
-      System.out.println("Preço: " + produto.getPrice());
-      System.out.println("-----------------------");
-    }
-  }
-
-  public void editProduct() {
-    Scanner num = new Scanner(System.in);
-    System.out.println("Digite o ID do produto que deseja editar:");
-    UUID id = UUID.fromString(num.next());
-
-    boolean found = false;
-    for (Product produto : listaDeProdutos) {
-      if (produto.getId().equals(id)) {
-        found = true;
-        System.out.println("Qual atributo você deseja editar? (nome/descrição/preço)");
-        String atributo = num.next();
-        switch (atributo.toLowerCase()) {
-          case "nome":
-            System.out.println("Digite o novo nome:");
-            produto.setName(num.nextLine());
-            System.out.println("Produto atualizado com sucesso!");
-            break;
-
-          case "descrição":
-            System.out.println("Digite a nova descrição:");
-            produto.setDescription(num.next());
-            System.out.println("Produto atualizado com sucesso!");
-            break;
-
-          case "preço":
-            System.out.println("Digite o novo preço:");
-            produto.setPrice(num.nextDouble());
-            System.out.println("Produto atualizado com sucesso!");
-            break;
-
-          default:
-            System.out.println("Atributo inválido!");
-            break;
-        }
-        break;
-      }
-    }
-    if (!found) {
-      System.out.println("Produto não encontrado!");
-    }
-  }
-
-  public void addProduct() {
-    Scanner num = new Scanner(System.in);
-    do {
-      System.out.println("Deseja adicionar um produto ao carrinho? (s/n)");
-      String resposta = num.next();
-      if (resposta.equalsIgnoreCase("s")) {
-        System.out.println("Digite o preço do produto:");
-        double price = num.nextDouble();
-        num.nextLine(); // consome o caractere de quebra de linha pendente
-        System.out.println("Digite o nome do produto:");
-        String name = num.nextLine();
-        System.out.println("Digite a descrição do produto:");
-        String description = num.nextLine();
-        Product produto = new Product();
-        produto.setName(name);
-        produto.setDescription(description);
-        produto.setId(UUID.randomUUID());
-        produto.setPrice(price);
-        listaDeProdutos.add(produto);
-        System.out.println("Produto adicionado com sucesso!");
-
-      } else if (resposta.equalsIgnoreCase("n")) {
-
-        break;
-      }
-
-    } while (true);
-  }
+  
 }
--- POO/Lista06/test/ProductManager.java
+++ POO/Lista06/test/ProductManager.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 
 public class ProductManager {
   private static List<Product> listaDeProdutos = new ArrayList<>();
-  
+
   public List<Product> getListaDeProdutos() {
     return listaDeProdutos;
   }
@@ -15,12 +15,13 @@ public class ProductManager {
       System.out.println("Deseja adicionar um produto ao carrinho? (s/n)");
       String resposta = num.next();
       if (resposta.equalsIgnoreCase("s")) {
+        System.out.println("Digite o preço do produto:");
+        double price = num.nextDouble();
+        num.nextLine(); // consome o caractere de quebra de linha pendente
         System.out.println("Digite o nome do produto:");
         String name = num.nextLine();
         System.out.println("Digite a descrição do produto:");
         String description = num.nextLine();
-        System.out.println("Digite o preço do produto:");
-        double price = num.nextDouble();
         Product produto = new Product();
         produto.setName(name);
         produto.setDescription(description);
@@ -30,10 +31,61 @@ public class ProductManager {
         System.out.println("Produto adicionado com sucesso!");
 
       } else if (resposta.equalsIgnoreCase("n")) {
-
         break;
       }
-
     } while (true);
+  }
+
+  public void editProduct() {
+    Scanner num = new Scanner(System.in);
+    System.out.println("Digite o ID do produto que deseja editar:");
+    UUID id = UUID.fromString(num.next());
+
+    boolean found = false;
+    for (Product produto : listaDeProdutos) {
+      if (produto.getId().equals(id)) {
+        found = true;
+        System.out.println("Qual atributo você deseja editar? (nome/descrição/preço)");
+        String atributo = num.next();
+        switch (atributo.toLowerCase()) {
+          case "nome":
+            System.out.println("Digite o novo nome:");
+            produto.setName(num.nextLine());
+            System.out.println("Produto atualizado com sucesso!");
+            break;
+
+          case "descrição":
+            System.out.println("Digite a nova descrição:");
+            produto.setDescription(num.next());
+            System.out.println("Produto atualizado com sucesso!");
+            break;
+
+          case "preço":
+            System.out.println("Digite o novo preço:");
+            produto.setPrice(num.nextDouble());
+            System.out.println("Produto atualizado com sucesso!");
+            break;
+
+          default:
+            System.out.println("Atributo inválido!");
+            break;
+        }
+        break;
+      }
+    }
+    if (!found) {
+      System.out.println("Produto não encontrado!");
+    }
+  }
+
+  public void showProducts() {
+    System.out.println("Produtos no carrinho:");
+    for (Product produto : listaDeProdutos) {
+      System.out.println("Nome: " + produto.getName());
+      System.out.println("Descrição: " + produto.getDescription());
+      System.out.println("ID: " + produto.getId());
+      System.out.println("Preço: " + produto.getPrice());
+      System.out.println("-----------------------");
+    }
   }
 }
